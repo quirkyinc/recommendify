e8241217a115baaa138f9afbeb892c879e1d7459
Conroy <conroy@quirky.com>
2012-11-16 17:06:16 -0500

    point to local hiredis.h instead of system

--- ext/recommendify.c
+++ ext/recommendify.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <hiredis/hiredis.h>
 
+#include "hiredis/hiredis.h"
 #include "version.h"
 #include "cc_item.h" 
 #include "jaccard.c" 
